c5e33fe4046589650abd9330b3e9a9e4c7e0fc3e
Wen He <wenhe@andrew.cmu.edu>
2018-09-10 03:11:32 +0000

    DONEgit add .git add .

--- q7.py
+++ q7.py
@@ -3,5 +3,5 @@ monthly_view = pandas.read_table('output', index_col=1, encoding='utf-8',
                                  header=None).iloc[:, 0]
 join_result = pandas.read_table('teams.txt', index_col=0, encoding='utf-8') \
             .join(monthly_view, how='inner')
-print(join_result.rank(method='dense', ascending=False, axis='index') \
-        .iloc[:, 0].to_json(double_precision=0))
+print(join_result.rank(method='dense', ascending=False, axis='index')
+      .iloc[:, 0].to_json(double_precision=0))
